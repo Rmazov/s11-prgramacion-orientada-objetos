587fadcb2626fb925a7785768a1d5879669af5e3
unknown <rmazov@hotmail.com>
2022-09-26 09:31:55 -0500

    first commit

---
+++ ejercicio.js
@@ -0,0 +1,21 @@
+class Estudiante {
+    constructor(nombre, edad, listaAsignatura ) {
+        this.nombre = nombre
+        this.edad = edad
+       this.listaAsignatura = listaAsignatura[{
+
+       }
+      
+      
+       ]
+       
+    }
+
+    obtenerDatos() {
+        console.log(`${this.nombre}, ${this.edad} ${this.listaAsignatura}`)
+    }
+}
+
+const nueva_estudiante = new Estudiante("Gorka", 34 ,{nombre: html})
+console.log(nueva_estudiante)
+nueva_estudiante.obtenerDatos()
